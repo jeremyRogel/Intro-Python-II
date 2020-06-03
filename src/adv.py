d2038b0db4e56f53ae16bea8f52ab99208cf6164
Jeremy Rogel <jeremyrogel@Jeremys-MBP.lan1>
2020-06-03 15:48:59 -0400

    revised files to comply with MVP

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,7 @@
 from room import Room
+from player import Player
+from item import Item
+import textwrap
 
 # Declare all the rooms
 
@@ -21,7 +24,14 @@ chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
 
+# declare items
 
+items = {
+    'sword': Item('Sword', 'description: Silver, One-Handed'),
+    'shield': Item('shield', 'description: Knight Symbol'),
+    'potion': Item('potion', 'description: Health Boost'),
+    'chest': Item('chest', 'description: Gold Treasure')
+}
 # Link rooms together
 
 room['outside'].n_to = room['foyer']
@@ -33,12 +43,18 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
+#add room items
+room['foyer'].room_items.append(items['sword'])
+room['overlook'].room_items.append(items['shield'])
+room['narrow'].room_items.append(items['potion'])
+room['treasure'].room_items.append(items['chest'])
+
 #
 # Main
 #
 
 # Make a new player object that is currently in the 'outside' room.
-
+player = Player('Jeremy', room['outside'])
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +65,65 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+print(f'\nWelcome to the Adventure Game!! :D\n')
+
+while True:
+    print(f'\nCurrent Room: {player.current_room.name}\n')
+    print(textwrap.TextWrapper().fill(text=player.current_room.description))
+    print(f'\n{player.current_room.current_items()}')
+
+    action = input('\nSelect the Direction You Would Like to Explore (n - North, s - South, e - East, w - West)\n\nCommands (get or take item name - Pickup, drop item name - Drop, i - Inventory, q - Quit)\n').split(" ")
+
+    #take and drop commands
+    if len(action) == 2:
+        if action[0] == 'get' or action[0] == 'take':
+            found = False
+            for i in range(len(player.current_room.room_items)):
+                item = player.current_room.room_items[i]
+                if action[1] == item.name:
+                    player.items.append(player.current_room.room_items.pop(i))
+                    item.on_take()
+                    found = True
+                    break
+            if not found:
+                print(f'\n{action[1]} not found in {player.current_room.name}.')
+        elif action[0] == 'drop':
+            found = False
+            for i in range(len(player.items)):
+                item = player.items[i]
+                if action[1] == item.name:
+                    player.current_room.room_items.append(player.items.pop(i))
+                    item.on_drop()
+                    found = True
+                    break
+            if not found:
+                print(f'\n{action[1]} not found in inventory.')
+
+    #Travel directions
+    elif action[0] == 'n':
+        if player.current_room.n_to == None:
+            print(f'\nYou cannot go that way!\n')
+        else: 
+            player.current_room = player.current_room.n_to
+    elif action[0] == 's':
+        if player.current_room.s_to == None:
+            print(f'\nYou cannot go that way!\n')
+        else:
+            player.current_room = player.current_room.s_to
+    elif action[0] == 'e':
+        if player.current_room.e_to == None:
+            print(f'\nYou cannot go that way!\n')
+        else:
+            player.current_room = player.current_room.e_to
+    elif action[0] == 'w':
+        if player.current_room.w_to == None:
+            print(f'\nYou cannot go that way!\n')
+        else:
+            player.current_room = player.current_room.w_to
+    elif action[0] == 'i' or action[0] == 'inventory':
+        print(player.current_inventory())
+    elif action[0] == 'q':
+        print('\nThanks for Playing!\n')
+        exit()
+    else: 
+        print('\nInvalid Direction!')  
